9d84aab6d60549a4a29b14b29eeeba9fac0e1aac
matteomarelli91 <matteomarelli91@gmail.com>
2024-10-01 15:09:37 +0200

    Decorator update

--- Pattern/DP/Decorator.go
+++ Pattern/DP/Decorator.go
@@ -23,3 +23,35 @@ func (e ExampleDecorator) Decorator(input string) string {
 func (e NumericDecorator) Decorator(input string) string {
 	return strconv.Itoa(e.Begin) + input + strconv.Itoa(e.End)
 }
+
+
+
+import (
+	"fmt"
+)
+
+type Decorator interface {
+	BasicPlot(inputS string) string
+}
+
+type Printer struct {
+	start   string
+	end     string
+	toDecor Decorator
+}
+
+func (s Printer) BasicPlot(inputS string) string {
+	if s.toDecor != nil {
+		return s.start + s.toDecor.BasicPlot(inputS) + s.end
+	} else {
+		return s.start + inputS + s.end
+	}
+}
+
+func main() {
+	decor1 := Printer{start: "{", end: "}"}
+	//decor2 := Printer{start: "[", end: "]", toDecor: decor1}
+	//decor3 := Brachet{start: "(", end: ")"}
+	fmt.Println(decor1.BasicPlot("pippo"))
+
+}
