b9ed3ef6a3142de1b28f1494f32831c7c13d3872
mmarelli <mmarelli@borgwarner.com>
2024-10-04 16:50:52 +0200

    Fix all issue found with test and updated test file

--- Codility/CaterpillarMethod/CountDistinctSlices/CountDistinctSlices.go
+++ Codility/CaterpillarMethod/CountDistinctSlices/CountDistinctSlices.go
@@ -18,45 +18,40 @@ func sumResults(begin int, end int) int {
 // "main" function to evaluate all slices with unique element in an array
 func Solution(M int, A []int) int {
 	occurrencies := MappingOccurrency(A)
-	firstCaterpillar := CaterpillarSlice(A, occurrencies, 0)
+	lenFullSlice := len(A) - 1
+	firstCaterpillar := CaterpillarSlice(A, occurrencies, 0, lenFullSlice)
 	var currentCaterpillar Caterpillar
 	result := 0
 	old_sum := 0
 	if len(A) > 1 {
 		i := 0
 		for i < len(A) {
-			if firstCaterpillar.begin == firstCaterpillar.end {
-				if firstCaterpillar.end < (len(A) - 1) {
-					result = result + 1
-					firstCaterpillar = CaterpillarSlice(A[firstCaterpillar.begin+1:], occurrencies, firstCaterpillar.begin+1)
-				}
-			} else {
-				currentCaterpillar = CaterpillarSlice(A[i:], occurrencies, i)
-				if currentCaterpillar.end < firstCaterpillar.end || firstCaterpillar.end < i {
-					if currentCaterpillar.end <= firstCaterpillar.end {
-						result = result + sumResults(firstCaterpillar.begin, currentCaterpillar.end)
-						old_sum = old_sum + sumResults(currentCaterpillar.begin+1, currentCaterpillar.end)
-						occurrencies[firstCaterpillar.number] = occurrencies[firstCaterpillar.number][1:]
-						if firstCaterpillar.number != currentCaterpillar.number {
-							occurrencies[currentCaterpillar.number] = occurrencies[currentCaterpillar.number][1:]
-						}
-						firstCaterpillar = CaterpillarSlice(A[currentCaterpillar.begin+1:], occurrencies, currentCaterpillar.begin+1)
-
-					} else {
-						result = result + sumResults(firstCaterpillar.begin, firstCaterpillar.end)
-						old_sum = old_sum + sumResults(firstCaterpillar.begin+1, firstCaterpillar.end)
-						occurrencies[firstCaterpillar.number] = occurrencies[firstCaterpillar.number][1:]
-						firstCaterpillar = CaterpillarSlice(A[firstCaterpillar.begin+1:], occurrencies, firstCaterpillar.begin+1)
-						i = firstCaterpillar.begin
+			currentCaterpillar = CaterpillarSlice(A[i:], occurrencies, i, lenFullSlice)
+			if currentCaterpillar.end < firstCaterpillar.end || firstCaterpillar.end < i {
+				if currentCaterpillar.end <= firstCaterpillar.end {
+					result = result + sumResults(firstCaterpillar.begin, currentCaterpillar.end)
+					old_sum = old_sum + sumResults(currentCaterpillar.begin+1, currentCaterpillar.end)
+					occurrencies[firstCaterpillar.number] = occurrencies[firstCaterpillar.number][1:]
+					if firstCaterpillar.number != currentCaterpillar.number {
+						occurrencies[currentCaterpillar.number] = occurrencies[currentCaterpillar.number][1:]
 					}
+					firstCaterpillar = CaterpillarSlice(A[currentCaterpillar.begin+1:], occurrencies, currentCaterpillar.begin+1, lenFullSlice)
+					i = currentCaterpillar.begin + 1
 
+				} else {
+					result = result + sumResults(firstCaterpillar.begin, firstCaterpillar.end)
+					old_sum = old_sum + sumResults(firstCaterpillar.begin+1, firstCaterpillar.end)
+					occurrencies[firstCaterpillar.number] = occurrencies[firstCaterpillar.number][1:]
+					firstCaterpillar = CaterpillarSlice(A[firstCaterpillar.begin+1:], occurrencies, firstCaterpillar.begin+1, lenFullSlice)
+					i = firstCaterpillar.begin
 				}
+
 			}
 			i++
 		}
-		if !(firstCaterpillar.begin == firstCaterpillar.end && firstCaterpillar.begin == 0) {
-			result = result + sumResults(firstCaterpillar.begin, firstCaterpillar.end) - old_sum
-		}
+		//if !(firstCaterpillar.begin == 1 && firstCaterpillar.end == lenFullSlice) {
+		result = result + sumResults(firstCaterpillar.begin, firstCaterpillar.end) - old_sum
+		//}
 		return result
 	} else {
 		return len(A)
@@ -74,14 +69,17 @@ func MappingOccurrency(A []int) map[int][]int {
 }
 
 // find the maximum slice length for a given number
-func CaterpillarSlice(A []int, OccurrencyMap map[int][]int, delta int) Caterpillar {
+func CaterpillarSlice(A []int, OccurrencyMap map[int][]int, delta int, lenFullSlice int) Caterpillar {
 	var currentCaterpillar Caterpillar
 	if len(A) > 0 {
 		currentnumber := A[0]
 		currentCaterpillar.number = currentnumber
 		if len(OccurrencyMap[currentnumber]) > 1 {
 			currentCaterpillar.begin = OccurrencyMap[currentnumber][0]
-			currentCaterpillar.end = OccurrencyMap[currentnumber][1] - 1
+			currentCaterpillar.end = OccurrencyMap[currentnumber][1]
+			if OccurrencyMap[currentnumber][1] < lenFullSlice || currentCaterpillar.begin == 0 {
+				currentCaterpillar.end = currentCaterpillar.end - 1
+			}
 		} else {
 			currentCaterpillar.begin = delta
 			currentCaterpillar.end = len(A) + delta - 1
--- Codility/CaterpillarMethod/CountDistinctSlices/CountDistinctSlices_test.go
+++ Codility/CaterpillarMethod/CountDistinctSlices/CountDistinctSlices_test.go
@@ -3,56 +3,55 @@ package CountDistinctSlices
 import (
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 // Test function CaterpillarSlice that check the longest slice with the first element not repeated. First check is done on a slice equal to [2,2], it should result
 // in a first call returning index 0 for begin and end and a second call returning 1 1. Second check done with [1,2] it should return 0,1
 func TestCaterpillar(t *testing.T) {
+	assert := assert.New(t)
+
+	ExpectedResul := Caterpillar{begin: 0, end: 0, number: 2}
 	fmt.Println(">>Test caterpillar func, find slice with not first element repeted. Check with []int{2, 2}")
 	input := []int{2, 2}
 	OccurrencyMap := MappingOccurrency(input)
-	control := CaterpillarSlice(input, OccurrencyMap, 0)
-	if control.begin != 0 || control.end != 0 {
-		t.Fatalf(`CaterpillarSlice([]int{2, 2},0) obtain begin = %v expected equal to 0 and obtain end = %v expected equal to 0`, control.begin, control.end)
-	}
-	control = CaterpillarSlice(input[1:], OccurrencyMap, 1)
-	if control.begin != 1 || control.end != 1 {
-		t.Fatalf(`CaterpillarSlice([]int{2},0) obtain begin = %v expected equal to 1 and obtain end = %v expected equal to 1`, control.begin, control.end)
-	}
+	control := CaterpillarSlice(input, OccurrencyMap, 0, 2)
+	assert.Equal(control, ExpectedResul, "Returned value must be as the one expected")
+
+	OccurrencyMap[control.number] = OccurrencyMap[control.number][1:]
+	control = CaterpillarSlice(input[1:], OccurrencyMap, 1, 2)
+	ExpectedResul = Caterpillar{begin: 1, end: 1, number: 2}
+	assert.Equal(control, ExpectedResul, "Returned value must be as the one expected")
 
 	fmt.Println(">>Test caterpillar func, find slice with not first element repeted. Check with []int{1, 2}")
 	input = []int{1, 2}
 	OccurrencyMap = MappingOccurrency(input)
-	control = CaterpillarSlice(input, OccurrencyMap, 0)
-	if control.begin != 0 || control.end != 1 {
-		t.Fatalf(`CaterpillarSlice(%v)) obtain begin = %v expected equal to 0 and obtain end = %v expected equal to 1`, input, control.begin, control.end)
-	}
+	control = CaterpillarSlice(input, OccurrencyMap, 0, 2)
+	ExpectedResul = Caterpillar{begin: 0, end: 1, number: 1}
+	assert.Equal(control, ExpectedResul, "Returned value must be as the one expected")
 
 }
 
 // Test function sumResults that calculate the combination between index begin and end
 func TestSumResults(t *testing.T) {
+	assert := assert.New(t)
+	solution := sumResults(0, 1)
+	assert.Equal(solution, 3, "Returned value must be as the one expected")
 
-	if sumResults(0, 1) != 3 {
-		t.Fatalf(`sumResults(0, 1) %v expect solution is 2`, sumResults(0, 1))
-	}
+	solution = sumResults(0, 0)
+	assert.Equal(solution, 1, "Returned value must be as the one expected")
 
-	if sumResults(0, 0) != 1 {
-		t.Fatalf(`sumResults(0, 0) %v expect solution is 1`, sumResults(0, 0))
-	}
-
-	if sumResults(1, 1) != 1 {
-		t.Fatalf(`sumResults(1, 1) %v expect solution is 1`, sumResults(1, 1))
-	}
+	solution = sumResults(1, 1)
+	assert.Equal(solution, 1, "Returned value must be as the one expected")
 }
 
 // test empty array
 func TestEmpty(t *testing.T) {
 	var input_empty []int
 	fmt.Printf(">>Test Zero: check empty array Solution(5, %v ) = %v\n", input_empty, Solution(5, input_empty))
-	if Solution(5, input_empty) != 0 {
-		t.Fatalf(`Solution(5, []int) %v expect solution is "", 0`, Solution(5, input_empty))
-	}
+	solution := Solution(5, input_empty)
+	assert.Equal(t, solution, 0, "Returned value must be as the one expected")
 }
 
 // test array with just two equal element
@@ -60,26 +59,22 @@ func TestOne(t *testing.T) {
 	input := []int{1}
 	solution := Solution(6, input)
 	fmt.Printf(">>First test: check empty array Solution(5, %v ) = %v\n", input, solution)
-	if solution != 1 {
-		t.Fatalf(`Solution(5, %v) %v expect solution is 1`, input, solution)
-	}
+	assert.Equal(t, solution, 1, "Returned value must be as the one expected")
 
 }
 
 // test array with just two different element
 func TestDouble(t *testing.T) {
+	assert := assert.New(t)
 	input := []int{1, 2}
 	solution := Solution(6, input)
 	fmt.Printf(">>Second test: check empty array Solution(5, %v ) = %v\n", input, solution)
-	if solution != 3 {
-		t.Fatalf(`Solution(5, []int{1, 2}) %v expect solution is 3`, solution)
-	}
+	assert.Equal(3, solution, "Returned value must be as the one expected")
+
 	input = []int{2, 2}
 	solution = Solution(2, input)
 	fmt.Printf(">>Second test: check empty array Solution(5, %v ) = %v\n", input, solution)
-	if solution != 2 {
-		t.Fatalf(`Solution(5, %v) %v expect solution is 2`, input, solution)
-	}
+	assert.Equal(2, solution, "Returned value must be as the one expected")
 
 }
 
@@ -88,10 +83,7 @@ func TestBaseCase(t *testing.T) {
 	input := []int{3, 4, 5, 5, 2}
 	solution := Solution(6, input)
 	fmt.Printf(">>Base test: check codility example array Solution(5, %v ) = %v\n", input, solution)
-	if solution != 9 {
-		t.Fatalf(`Solution(5,  %v) %v expect solution is 9`, input, solution)
-	}
-
+	assert.Equal(t, 9, solution, "Returned value must be as the one expected")
 }
 
 // launch a test case with a longer array
@@ -100,9 +92,7 @@ func TestFunctionalCase0(t *testing.T) {
 	input := []int{5, 4, 7, 5, 7, 9, 2}
 	solution := Solution(9, input)
 	fmt.Printf(">>Check array Solution(9, %v ) = %v\n", input, solution)
-	if solution != 18 {
-		t.Fatalf(`Solution(5, %v) %v expect solution is 18`, input, solution)
-	}
+	assert.Equal(t, 18, solution, "Returned value must be as the one expected")
 }
 
 func TestFunctionalCase1(t *testing.T) {
@@ -110,8 +100,6 @@ func TestFunctionalCase1(t *testing.T) {
 	input := []int{5, 4, 7, 5, 7, 9, 2, 3, 8, 7, 12, 2, 22, 15, 18, 2, 8}
 	solution := Solution(22, input)
 	fmt.Printf(">>Check array Solution(22, %v ) = %v\n", input, solution)
+	assert.Equal(t, 75, solution, "Returned value must be as the one expected")
 
-	if Solution(6, input) != 51 {
-		t.Fatalf(`Solution(5, %v) %v expect solution is 51`, input, solution)
-	}
 }
